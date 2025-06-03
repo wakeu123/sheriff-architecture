0e3324f9d3d2839c0a2a0c97ebfb60d202abde13
wakeu123 <georgeswakeu21@gmail.com>
2025-06-03 08:24:25 +0100

    u

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { ButtonModule } from 'primeng/button';
 import { Component, OnInit } from '@angular/core';
 import { ValidationErrors } from '@angular/forms';
-import { GlobalLoaderComponent } from '@domains/shared/components/global-loader/global-loader.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ButtonModule, GlobalLoaderComponent],
+  imports: [ButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-  
+
   //pm i @ngspot/ngx-errors très important pour gérer les erreurs des formulaires
   title = 'sheriff-architecture';
 
