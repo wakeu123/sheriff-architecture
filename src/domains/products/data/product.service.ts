9800536cbfd9c2626667730d1a3601e27d7aba99
wakeu123 <georgeswakeu21@gmail.com>
2025-06-26 12:08:56 +0100

    u

---
+++ src/domains/products/data/product.service.ts
@@ -0,0 +1,7 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+
+@Injectable()
+export class ProductService {
+  private readonly _http = inject(HttpClient);
+}
